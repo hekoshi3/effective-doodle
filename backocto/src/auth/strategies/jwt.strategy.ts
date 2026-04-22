e052290ffee7dfdd4b8b94aa3f70fdbb8501ffb8
hekoshi <47317489+hekoshi3@users.noreply.github.com>
2026-04-22 22:29:47 +0700

    chore!: gitignore and chore in strategy script

--- backocto/src/auth/strategies/jwt.strategy.ts
+++ backocto/src/auth/strategies/jwt.strategy.ts
@@ -16,7 +16,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   validate(payload: any) {
-    console.log('--- JWT PAYLOAD ---', payload);
     return { userId: payload.sub, username: payload.username };
   }
 }
