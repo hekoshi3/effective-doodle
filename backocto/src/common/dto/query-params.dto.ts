126b5bb90b17de012974dcb958fe52751446b78a
hekoshi <47317489+hekoshi3@users.noreply.github.com>
2026-05-04 20:45:51 +0700

    feat: add query params dto

---
+++ backocto/src/common/dto/query-params.dto.ts
@@ -0,0 +1,11 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class BaseQueryDto {
+  @IsOptional() @IsString() ordering?: string;
+  @IsOptional() @IsString() author?: string;
+  @IsOptional() @IsString() tag?: string;
+  @IsOptional() @IsString() created_after?: string;
+  @IsOptional() @IsString() feed?: string;
+  @IsOptional() @IsString() model_type?: string;
+  @IsOptional() @IsString() download_count?: string;
+}
